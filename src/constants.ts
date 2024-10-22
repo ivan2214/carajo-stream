ed3ee2c4ada5c1a60ce014c309cf396ecc4c0a66
ivan2214 <bongiovanniivan12@gmail.com>
2024-10-22 20:23:48 -0300

    feat: Set up project structure
    Initial setup with Tailwind CSS, Shadcn UI, and project structure.

---
+++ src/constants.ts
@@ -0,0 +1,239 @@
+import type { CategorizedVideos } from "@/types";
+
+export const datos: CategorizedVideos = {
+	shorts: [
+		{
+			videoId: "uMa824cVpMY",
+			publishedAt: "2024-10-21T20:44:41Z",
+			title:
+				"MILEI TERMINA CON LA DEUDA EN ARGENTINA #podcast #milei #mileipresidente #lamisa",
+			videoType: "short",
+			description: "",
+			formattedDuration: "01:00",
+			url: "https://www.youtube.com/watch?v=uMa824cVpMY",
+			channelTitle: "Carajo",
+			thumbnails: {
+				default: {
+					url: "https://i.ytimg.com/vi/uMa824cVpMY/default.jpg",
+					width: 120,
+					height: 90,
+				},
+				medium: {
+					url: "https://i.ytimg.com/vi/uMa824cVpMY/mqdefault.jpg",
+					width: 320,
+					height: 180,
+				},
+				high: {
+					url: "https://i.ytimg.com/vi/uMa824cVpMY/hqdefault.jpg",
+					width: 480,
+					height: 360,
+				},
+				standard: {
+					url: "https://i.ytimg.com/vi/uMa824cVpMY/sddefault.jpg",
+					width: 640,
+					height: 480,
+				},
+				maxres: {
+					url: "https://i.ytimg.com/vi/uMa824cVpMY/maxresdefault.jpg",
+					width: 1280,
+					height: 720,
+				},
+			},
+		},
+	],
+	videos: [
+		{
+			videoId: "TwbZGLLHKGw",
+			publishedAt: "2024-10-22T18:40:06Z",
+			title: "MILEI CIERRA LA AFIP",
+			videoType: "video",
+			description:
+				"SI LA VES SUSCRIBITE ACÁ: https://bit.ly/3KUwDfq \n\nSEGUINOS YA\n⚈  https://www.instagram.com/carajostream/\n⚈  https://twitter.com/carajostream\n⚈  https://www.tiktok.com/@carajostream",
+			formattedDuration: "15:16",
+			url: "https://www.youtube.com/watch?v=TwbZGLLHKGw",
+			channelTitle: "Carajo",
+			thumbnails: {
+				default: {
+					url: "https://i.ytimg.com/vi/TwbZGLLHKGw/default.jpg",
+					width: 120,
+					height: 90,
+				},
+				medium: {
+					url: "https://i.ytimg.com/vi/TwbZGLLHKGw/mqdefault.jpg",
+					width: 320,
+					height: 180,
+				},
+				high: {
+					url: "https://i.ytimg.com/vi/TwbZGLLHKGw/hqdefault.jpg",
+					width: 480,
+					height: 360,
+				},
+				standard: {
+					url: "https://i.ytimg.com/vi/TwbZGLLHKGw/sddefault.jpg",
+					width: 640,
+					height: 480,
+				},
+				maxres: {
+					url: "https://i.ytimg.com/vi/TwbZGLLHKGw/maxresdefault.jpg",
+					width: 1280,
+					height: 720,
+				},
+			},
+		},
+		{
+			videoId: "5uYcXCm3Zwc",
+			publishedAt: "2024-10-21T23:11:31Z",
+			title: "ADIÓS AL DECRETO PRO T3RR0R15MO DE NESTOR KIRCHNER",
+			videoType: "video",
+			description:
+				"SI LA VES SUSCRIBITE ACÁ: https://bit.ly/3KUwDfq \n\nSEGUINOS YA\n⚈  https://www.instagram.com/carajostream/\n⚈  https://twitter.com/carajostream\n⚈  https://www.tiktok.com/@carajostream",
+			formattedDuration: "07:26",
+			url: "https://www.youtube.com/watch?v=5uYcXCm3Zwc",
+			channelTitle: "Carajo",
+			thumbnails: {
+				default: {
+					url: "https://i.ytimg.com/vi/5uYcXCm3Zwc/default.jpg",
+					width: 120,
+					height: 90,
+				},
+				medium: {
+					url: "https://i.ytimg.com/vi/5uYcXCm3Zwc/mqdefault.jpg",
+					width: 320,
+					height: 180,
+				},
+				high: {
+					url: "https://i.ytimg.com/vi/5uYcXCm3Zwc/hqdefault.jpg",
+					width: 480,
+					height: 360,
+				},
+				standard: {
+					url: "https://i.ytimg.com/vi/5uYcXCm3Zwc/sddefault.jpg",
+					width: 640,
+					height: 480,
+				},
+				maxres: {
+					url: "https://i.ytimg.com/vi/5uYcXCm3Zwc/maxresdefault.jpg",
+					width: 1280,
+					height: 720,
+				},
+			},
+		},
+		{
+			videoId: "KoF3wp1PnU4",
+			publishedAt: "2024-10-21T16:30:59Z",
+			title: "PELIGRO AL DORMIR: LA PARÁLISIS DEL SUEÑO",
+			videoType: "video",
+			description:
+				"SI LA VES SUSCRIBITE ACÁ: https://bit.ly/3KUwDfq \n\nSEGUINOS YA\n⚈  https://www.instagram.com/carajostream/\n⚈  https://twitter.com/carajostream\n⚈  https://www.tiktok.com/@carajostream",
+			formattedDuration: "23:25",
+			url: "https://www.youtube.com/watch?v=KoF3wp1PnU4",
+			channelTitle: "Carajo",
+			thumbnails: {
+				default: {
+					url: "https://i.ytimg.com/vi/KoF3wp1PnU4/default.jpg",
+					width: 120,
+					height: 90,
+				},
+				medium: {
+					url: "https://i.ytimg.com/vi/KoF3wp1PnU4/mqdefault.jpg",
+					width: 320,
+					height: 180,
+				},
+				high: {
+					url: "https://i.ytimg.com/vi/KoF3wp1PnU4/hqdefault.jpg",
+					width: 480,
+					height: 360,
+				},
+				standard: {
+					url: "https://i.ytimg.com/vi/KoF3wp1PnU4/sddefault.jpg",
+					width: 640,
+					height: 480,
+				},
+				maxres: {
+					url: "https://i.ytimg.com/vi/KoF3wp1PnU4/maxresdefault.jpg",
+					width: 1280,
+					height: 720,
+				},
+			},
+		},
+		{
+			videoId: "0t70DVv60kc",
+			publishedAt: "2024-10-20T23:00:06Z",
+			title: "¿QUE ESTA PASANDO EN BOLIVIA? LO QUE LOS MEDIOS ESCONDEN",
+			videoType: "video",
+			description:
+				"SI LA VES SUSCRIBITE ACÁ: https://bit.ly/3KUwDfq \n\nSEGUINOS YA\n⚈  https://www.instagram.com/carajostream/\n⚈  https://twitter.com/carajostream\n⚈  https://www.tiktok.com/@carajostream",
+			formattedDuration: "21:50",
+			url: "https://www.youtube.com/watch?v=0t70DVv60kc",
+			channelTitle: "Carajo",
+			thumbnails: {
+				default: {
+					url: "https://i.ytimg.com/vi/0t70DVv60kc/default.jpg",
+					width: 120,
+					height: 90,
+				},
+				medium: {
+					url: "https://i.ytimg.com/vi/0t70DVv60kc/mqdefault.jpg",
+					width: 320,
+					height: 180,
+				},
+				high: {
+					url: "https://i.ytimg.com/vi/0t70DVv60kc/hqdefault.jpg",
+					width: 480,
+					height: 360,
+				},
+				standard: {
+					url: "https://i.ytimg.com/vi/0t70DVv60kc/sddefault.jpg",
+					width: 640,
+					height: 480,
+				},
+				maxres: {
+					url: "https://i.ytimg.com/vi/0t70DVv60kc/maxresdefault.jpg",
+					width: 1280,
+					height: 720,
+				},
+			},
+		},
+		{
+			videoId: "dYV8iCoulZ0",
+			publishedAt: "2024-10-19T23:00:06Z",
+			title: "PROFESOR EXPLICA LAS CRUZADAS | PARTE 2",
+			videoType: "video",
+			description:
+				"SI LA VES SUSCRIBITE ACÁ: https://bit.ly/3KUwDfq \n\nSEGUINOS YA\n⚈  https://www.instagram.com/carajostream/\n⚈  https://twitter.com/carajostream\n⚈  https://www.tiktok.com/@carajostream",
+			formattedDuration: "20:05",
+			url: "https://www.youtube.com/watch?v=dYV8iCoulZ0",
+			channelTitle: "Carajo",
+			thumbnails: {
+				default: {
+					url: "https://i.ytimg.com/vi/dYV8iCoulZ0/default.jpg",
+					width: 120,
+					height: 90,
+				},
+				medium: {
+					url: "https://i.ytimg.com/vi/dYV8iCoulZ0/mqdefault.jpg",
+					width: 320,
+					height: 180,
+				},
+				high: {
+					url: "https://i.ytimg.com/vi/dYV8iCoulZ0/hqdefault.jpg",
+					width: 480,
+					height: 360,
+				},
+				standard: {
+					url: "https://i.ytimg.com/vi/dYV8iCoulZ0/sddefault.jpg",
+					width: 640,
+					height: 480,
+				},
+				maxres: {
+					url: "https://i.ytimg.com/vi/dYV8iCoulZ0/maxresdefault.jpg",
+					width: 1280,
+					height: 720,
+				},
+			},
+		},
+	],
+	upcomings: [],
+	live: null,
+	streams: [],
+};
---
+++ src/types.ts
@@ -0,0 +1,79 @@
+export interface CategorizedVideos {
+	shorts: VideoItem[];
+	videos: VideoItem[];
+	live?: VideoItem | null;
+	upcomings: VideoItem[];
+	streams: VideoItem[];
+}
+
+export interface VideoItem {
+	videoId: string;
+	publishedAt: string;
+	title: string;
+	description: string;
+	formattedDuration: string;
+	url: string;
+	channelTitle: string;
+	thumbnails: Thumbnails;
+	videoType: "short" | "video" | "live" | "upcoming" | "streams";
+}
+
+// esto deevuelve la api de youtube para todos
+
+export interface ResponseYoutube {
+	items: YoutubeItem[];
+}
+
+export interface YoutubeItem {
+	kind: string;
+	etag: string;
+	id: {
+		videoId: string;
+	};
+	snippet: Snippet;
+}
+
+export interface Snippet {
+	publishedAt: string;
+	channelId: string;
+	title: string;
+	description: string;
+	thumbnails: Thumbnails;
+	channelTitle: string;
+	tags: string[];
+	categoryId: string;
+	liveBroadcastContent: string;
+	localized: { title: string; description: string };
+	defaultAudioLanguage: string;
+}
+
+export interface Thumbnails {
+	default: { url: string; width: number; height: number };
+	medium: { url: string; width: number; height: number };
+	high: { url: string; width: number; height: number };
+	standard: { url: string; width: number; height: number };
+	maxres: { url: string; width: number; height: number };
+}
+
+// esto devuelve la api de youtube con detalles
+
+export interface ResponseVideoDetails {
+	items: Item[];
+}
+
+export interface Item {
+	kind: string;
+	etag: string;
+	id: string;
+	snippet: Snippet;
+	contentDetails: {
+		duration: string;
+		dimension: string;
+		definition: string;
+		caption: string;
+		licensedContent: boolean;
+		// biome-ignore lint/complexity/noBannedTypes: <explanation>
+		contentRating: {};
+		projection: string;
+	};
+}
