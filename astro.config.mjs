2bb9f359a96eaf81d8255633a3b9b17854cc9f9d
ivan2214 <bongiovanniivan12@gmail.com>
2024-10-23 14:30:23 -0300

    feat: Enable server-side rendering
    Enabled server-side rendering for improved SEO and performance.
    
    SEO, Performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://carajo-stream.vercel.app",
+
 	integrations: [
 		react(),
 		tailwind({
@@ -17,4 +20,11 @@ export default defineConfig({
 		}),
 		sitemap(),
 	],
+
+	output: "server",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 });
