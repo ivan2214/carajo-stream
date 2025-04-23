fa038724597e49afa2b25dfe5c0334598d65c63d
ivan2214 <bongiovanniivan12@gmail.com>
2025-04-23 19:50:53 -0300

    refactor(schedule): update schedule section to use PROGRAMS constant and add program images
    The schedule section has been refactored to use the PROGRAMS constant for displaying program details. Additionally, program images have been integrated into the schedule cards to enhance the visual representation of each program. This change improves maintainability and provides a more engaging user experience.

--- src/constants.ts
+++ src/constants.ts
@@ -1,239 +1,115 @@
-import type { CategorizedVideos } from "@/types";
+// array con los programas que van a ser streameados
 
-export const datos: CategorizedVideos = {
-	shorts: [
-		{
-			videoId: "uMa824cVpMY",
-			publishedAt: "2024-10-21T20:44:41Z",
-			title:
-				"MILEI TERMINA CON LA DEUDA EN ARGENTINA #podcast #milei #mileipresidente #lamisa",
-			videoType: "short",
-			description: "",
-			formattedDuration: "01:00",
-			url: "https://www.youtube.com/watch?v=uMa824cVpMY",
-			channelTitle: "Carajo",
-			thumbnails: {
-				default: {
-					url: "https://i.ytimg.com/vi/uMa824cVpMY/default.jpg",
-					width: 120,
-					height: 90,
-				},
-				medium: {
-					url: "https://i.ytimg.com/vi/uMa824cVpMY/mqdefault.jpg",
-					width: 320,
-					height: 180,
-				},
-				high: {
-					url: "https://i.ytimg.com/vi/uMa824cVpMY/hqdefault.jpg",
-					width: 480,
-					height: 360,
-				},
-				standard: {
-					url: "https://i.ytimg.com/vi/uMa824cVpMY/sddefault.jpg",
-					width: 640,
-					height: 480,
-				},
-				maxres: {
-					url: "https://i.ytimg.com/vi/uMa824cVpMY/maxresdefault.jpg",
-					width: 1280,
-					height: 720,
-				},
-			},
-		},
-	],
-	videos: [
-		{
-			videoId: "TwbZGLLHKGw",
-			publishedAt: "2024-10-22T18:40:06Z",
-			title: "MILEI CIERRA LA AFIP",
-			videoType: "video",
-			description:
-				"SI LA VES SUSCRIBITE ACÁ: https://bit.ly/3KUwDfq \n\nSEGUINOS YA\n⚈  https://www.instagram.com/carajostream/\n⚈  https://twitter.com/carajostream\n⚈  https://www.tiktok.com/@carajostream",
-			formattedDuration: "15:16",
-			url: "https://www.youtube.com/watch?v=TwbZGLLHKGw",
-			channelTitle: "Carajo",
-			thumbnails: {
-				default: {
-					url: "https://i.ytimg.com/vi/TwbZGLLHKGw/default.jpg",
-					width: 120,
-					height: 90,
-				},
-				medium: {
-					url: "https://i.ytimg.com/vi/TwbZGLLHKGw/mqdefault.jpg",
-					width: 320,
-					height: 180,
-				},
-				high: {
-					url: "https://i.ytimg.com/vi/TwbZGLLHKGw/hqdefault.jpg",
-					width: 480,
-					height: 360,
-				},
-				standard: {
-					url: "https://i.ytimg.com/vi/TwbZGLLHKGw/sddefault.jpg",
-					width: 640,
-					height: 480,
-				},
-				maxres: {
-					url: "https://i.ytimg.com/vi/TwbZGLLHKGw/maxresdefault.jpg",
-					width: 1280,
-					height: 720,
-				},
-			},
-		},
-		{
-			videoId: "5uYcXCm3Zwc",
-			publishedAt: "2024-10-21T23:11:31Z",
-			title: "ADIÓS AL DECRETO PRO T3RR0R15MO DE NESTOR KIRCHNER",
-			videoType: "video",
-			description:
-				"SI LA VES SUSCRIBITE ACÁ: https://bit.ly/3KUwDfq \n\nSEGUINOS YA\n⚈  https://www.instagram.com/carajostream/\n⚈  https://twitter.com/carajostream\n⚈  https://www.tiktok.com/@carajostream",
-			formattedDuration: "07:26",
-			url: "https://www.youtube.com/watch?v=5uYcXCm3Zwc",
-			channelTitle: "Carajo",
-			thumbnails: {
-				default: {
-					url: "https://i.ytimg.com/vi/5uYcXCm3Zwc/default.jpg",
-					width: 120,
-					height: 90,
-				},
-				medium: {
-					url: "https://i.ytimg.com/vi/5uYcXCm3Zwc/mqdefault.jpg",
-					width: 320,
-					height: 180,
-				},
-				high: {
-					url: "https://i.ytimg.com/vi/5uYcXCm3Zwc/hqdefault.jpg",
-					width: 480,
-					height: 360,
-				},
-				standard: {
-					url: "https://i.ytimg.com/vi/5uYcXCm3Zwc/sddefault.jpg",
-					width: 640,
-					height: 480,
-				},
-				maxres: {
-					url: "https://i.ytimg.com/vi/5uYcXCm3Zwc/maxresdefault.jpg",
-					width: 1280,
-					height: 720,
-				},
-			},
-		},
-		{
-			videoId: "KoF3wp1PnU4",
-			publishedAt: "2024-10-21T16:30:59Z",
-			title: "PELIGRO AL DORMIR: LA PARÁLISIS DEL SUEÑO",
-			videoType: "video",
-			description:
-				"SI LA VES SUSCRIBITE ACÁ: https://bit.ly/3KUwDfq \n\nSEGUINOS YA\n⚈  https://www.instagram.com/carajostream/\n⚈  https://twitter.com/carajostream\n⚈  https://www.tiktok.com/@carajostream",
-			formattedDuration: "23:25",
-			url: "https://www.youtube.com/watch?v=KoF3wp1PnU4",
-			channelTitle: "Carajo",
-			thumbnails: {
-				default: {
-					url: "https://i.ytimg.com/vi/KoF3wp1PnU4/default.jpg",
-					width: 120,
-					height: 90,
-				},
-				medium: {
-					url: "https://i.ytimg.com/vi/KoF3wp1PnU4/mqdefault.jpg",
-					width: 320,
-					height: 180,
-				},
-				high: {
-					url: "https://i.ytimg.com/vi/KoF3wp1PnU4/hqdefault.jpg",
-					width: 480,
-					height: 360,
-				},
-				standard: {
-					url: "https://i.ytimg.com/vi/KoF3wp1PnU4/sddefault.jpg",
-					width: 640,
-					height: 480,
-				},
-				maxres: {
-					url: "https://i.ytimg.com/vi/KoF3wp1PnU4/maxresdefault.jpg",
-					width: 1280,
-					height: 720,
-				},
-			},
-		},
-		{
-			videoId: "0t70DVv60kc",
-			publishedAt: "2024-10-20T23:00:06Z",
-			title: "¿QUE ESTA PASANDO EN BOLIVIA? LO QUE LOS MEDIOS ESCONDEN",
-			videoType: "video",
-			description:
-				"SI LA VES SUSCRIBITE ACÁ: https://bit.ly/3KUwDfq \n\nSEGUINOS YA\n⚈  https://www.instagram.com/carajostream/\n⚈  https://twitter.com/carajostream\n⚈  https://www.tiktok.com/@carajostream",
-			formattedDuration: "21:50",
-			url: "https://www.youtube.com/watch?v=0t70DVv60kc",
-			channelTitle: "Carajo",
-			thumbnails: {
-				default: {
-					url: "https://i.ytimg.com/vi/0t70DVv60kc/default.jpg",
-					width: 120,
-					height: 90,
-				},
-				medium: {
-					url: "https://i.ytimg.com/vi/0t70DVv60kc/mqdefault.jpg",
-					width: 320,
-					height: 180,
-				},
-				high: {
-					url: "https://i.ytimg.com/vi/0t70DVv60kc/hqdefault.jpg",
-					width: 480,
-					height: 360,
-				},
-				standard: {
-					url: "https://i.ytimg.com/vi/0t70DVv60kc/sddefault.jpg",
-					width: 640,
-					height: 480,
-				},
-				maxres: {
-					url: "https://i.ytimg.com/vi/0t70DVv60kc/maxresdefault.jpg",
-					width: 1280,
-					height: 720,
-				},
-			},
-		},
-		{
-			videoId: "dYV8iCoulZ0",
-			publishedAt: "2024-10-19T23:00:06Z",
-			title: "PROFESOR EXPLICA LAS CRUZADAS | PARTE 2",
-			videoType: "video",
-			description:
-				"SI LA VES SUSCRIBITE ACÁ: https://bit.ly/3KUwDfq \n\nSEGUINOS YA\n⚈  https://www.instagram.com/carajostream/\n⚈  https://twitter.com/carajostream\n⚈  https://www.tiktok.com/@carajostream",
-			formattedDuration: "20:05",
-			url: "https://www.youtube.com/watch?v=dYV8iCoulZ0",
-			channelTitle: "Carajo",
-			thumbnails: {
-				default: {
-					url: "https://i.ytimg.com/vi/dYV8iCoulZ0/default.jpg",
-					width: 120,
-					height: 90,
-				},
-				medium: {
-					url: "https://i.ytimg.com/vi/dYV8iCoulZ0/mqdefault.jpg",
-					width: 320,
-					height: 180,
-				},
-				high: {
-					url: "https://i.ytimg.com/vi/dYV8iCoulZ0/hqdefault.jpg",
-					width: 480,
-					height: 360,
-				},
-				standard: {
-					url: "https://i.ytimg.com/vi/dYV8iCoulZ0/sddefault.jpg",
-					width: 640,
-					height: 480,
-				},
-				maxres: {
-					url: "https://i.ytimg.com/vi/dYV8iCoulZ0/maxresdefault.jpg",
-					width: 1280,
-					height: 720,
-				},
-			},
-		},
-	],
-	upcomings: [],
-	live: null,
-	streams: [],
-};
+export interface Streamer {
+  name: string;
+  networks: {
+    name: string;
+    href: string;
+    user: string;
+    avatar: string;
+  }[];
+  isConductor: boolean;
+}
+
+export interface Program {
+  title: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  image: string;
+  streamers: Streamer[];
+}
+
+export const PROGRAMS: Program[] = [
+  {
+    title: "La trinchera",
+    date: "Lunes a viernes",
+    startTime: "09:00",
+    endTime: "11:00",
+    image: "/images/la-trinchera.webp",
+    streamers: [
+      {
+        name: "Nicolás Promanzio",
+        networks: [
+          {
+            name: "X",
+            href: "https://x.com/nicpromanzio",
+            user: "@nicpromanzio",
+            avatar: "/images/streamers/nicolas.webp",
+          },
+        ],
+        isConductor: true,
+      },
+    ],
+  },
+  {
+    title: "La Fuerza",
+    date: "Lunes a viernes",
+    startTime: "11:00",
+    endTime: "13:00",
+    image: "/images/la-fuerza.webp",
+    streamers: [
+      {
+        name: "",
+        networks: [],
+        isConductor: true,
+      },
+    ],
+  },
+  {
+    title: "Estado de sitio",
+    date: "Miercoles y Viernes",
+    startTime: "17:00",
+    endTime: "19:00",
+    image: "/images/estado-de-sitio.webp",
+    streamers: [
+      {
+        name: "",
+        networks: [],
+        isConductor: true,
+      },
+    ],
+  },
+  {
+    title: "Alta suciedad",
+    date: "Lunes, Miercoles y Viernes",
+    startTime: "19:00",
+    endTime: "21:00",
+    image: "/images/alta-suciedad.webp",
+    streamers: [
+      {
+        name: "Piro",
+        networks: [
+          {
+            name: "X",
+            href: "https://x.com/piroabrazo",
+            user: "@piroabrazo",
+            avatar: "/images/streamers/piro.webp",
+          },
+        ],
+        isConductor: true,
+      },
+    ],
+  },
+  {
+    title: "La Misa",
+    date: "Lunes a viernes",
+    startTime: "21:00",
+    endTime: "23:00",
+    image: "/images/la-misa.webp",
+    streamers: [
+      {
+        name: "El Gordo Dan",
+        networks: [
+          {
+            name: "X",
+            href: "https://x.com/GordoDan_",
+            user: "@GordoDan_",
+            avatar: "/images/streamers/el-gordo-dan.webp",
+          },
+        ],
+        isConductor: true,
+      },
+    ],
+  },
+];
