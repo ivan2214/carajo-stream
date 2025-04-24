fb08938779dcd3f997625d5f659d8b01f7dabdfa
ivan2214 <bongiovanniivan12@gmail.com>
2025-04-23 22:05:12 -0300

    feat: update tailwindcss v4 and shadcn with tw v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,27 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
 import vercel from "@astrojs/vercel";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://carajo-stream.vercel.app",
-
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-  ],
-
+  integrations: [react(), sitemap()],
   output: "server",
+
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
